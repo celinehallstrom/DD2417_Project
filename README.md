e8bdc913b6339b5c8d54ca723c4d3b1ce8edfa41
Celine Helgesson Hallström <celinehhallstrom@gmail.com>
2022-05-18 09:23:58 +0200

    model function created

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # DD2417_project
 
-# Create feature vectors
+# Create feature vectors - only needed first time
 Go to the folder data and:
 Run the functions XX and YY in get_features.py (this takes a long time)
 
+# Data
+Put the following files in a folder called data
+feature_vectors.csv
+question_pairs_final.csv 
+w2v_1.csv
+Positive and Negative Word List.ods
 
-# Run models 
-To be written
+# Test models 
+Select which model to run in the run_model.py file. 
--- run_model.py
+++ run_model.py
@@ -8,18 +8,23 @@ import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix, log_loss, accuracy_score
 from get_features import create_final_df
 
+def test_models(model='LogisticRegression', w_1=0.5):
+    _, df_features, labels = create_final_df()
 
-df_new, df_features, labels = create_final_df()
+    print('Number of datapoints: ', len(df_features))
+    X_train, X_test, y_train, y_test = train_test_split(df_features, labels, test_size=0.2, random_state=42)
+    if model == 'LogisticRegression':
+        a = w_1
+        s_weight = np.where(y_train == 1, a, 1-a)
+        clf = LogisticRegression(random_state=42).fit(X_train, y_train, sample_weight=s_weight)
+    elif model == 'SGDClassifier':
+        clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=100).fit(X_train, y_train)
+    elif model == 'SVC':
+        clf = SVC(gamma='auto').fit(X_train, y_train)
+    preds = clf.predict(X_test)
+    print('Confusion matrix: \n', confusion_matrix(y_test, preds))
+    print('Accuracy', accuracy_score(y_test, preds))
+    print(classification_report(y_test, preds, target_names = ['0', '1']))
 
-print('Number of datapoints: ', len(df_features))
-X_train, X_test, y_train, y_test = train_test_split(df_features, labels, test_size=0.2, random_state=42)
-#print(df_features)
-#clf = SVC(gamma='auto').fit(X_train, y_train)
-#clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=100).fit(X_train, y_train)
-a = 0.5
-s_weight = np.where(y_train == 1, a, 1-a)
-clf = LogisticRegression(random_state=42).fit(X_train, y_train, sample_weight=s_weight)
-preds = clf.predict(X_test)
-print('Confusion matrix: \n', confusion_matrix(y_test, preds))
-print('Accuracy', accuracy_score(y_test, preds))
-print(classification_report(y_test, preds, target_names = ['0', '1']))
+model = ['LogisticRegression', 'SGDClassifier', 'SVC']
+test_models(model[0])
